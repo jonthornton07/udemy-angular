8bfb2ed60b1831ce07fa4945ce7edf73cd8cc2aa
Jon Thornton <jon.thornton07@gmail.com>
2021-06-16 18:38:40 -0400

    Section 11: end

--- main-project/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ main-project/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,19 +1,34 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Recipe} from "../recipe.model";
 import {RecipeService} from "../recipe.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
   styleUrls: ['./recipe-detail.component.css']
 })
-export class RecipeDetailComponent {
+export class RecipeDetailComponent implements OnInit {
 
   @Input() recipe: Recipe
+  id: number
 
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService,
+              private route: ActivatedRoute,
+              private router: Router) { }
 
-  toShoppingList() {
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.id = +params['id']
+      this.recipe = this.recipeService.getRecipe(this.id)
+    })
+  }
+
+  onAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients)
+  }
+
+  onEditRecipe() {
+    this.router.navigate(['../', this.id, 'edit'], {relativeTo: this.route})
   }
 }
