fd39fc7570def54217b24eee313a1df50544359e
Jon Thornton <jon.thornton07@gmail.com>
2021-06-23 07:02:36 -0400

    Middle commit

--- http/src/app/app.component.ts
+++ http/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import {Post} from './post.model';
 
 @Component({
   selector: 'app-root',
@@ -8,21 +10,53 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
   loadedPosts = [];
+  isFetching = false;
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
     // Send Http request
-    console.log(postData);
+    this.http.post<{ name: string }>(
+      'https://ng-complete-guide-d92be-default-rtdb.firebaseio.com/posts.json',
+      postData
+    ).subscribe(responseData => {
+      console.log(responseData);
+    });
   }
 
   onFetchPosts() {
-    // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
-    // Send Http request
+    this.http
+      .delete('https://ng-complete-guide-d92be-default-rtdb.firebaseio.com/posts.json')
+      .subscribe(responseData => {
+        console.log(responseData)
+      })
+  }
+
+  private fetchPosts() {
+    this.isFetching = true;
+    this.http
+      .get<Post>('https://ng-complete-guide-d92be-default-rtdb.firebaseio.com/posts.json')
+      .pipe(map(responseData => {
+        const postArray: Post[] = [];
+
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            postArray.push({...responseData[key], id: key})
+          }
+        }
+        return postArray
+      }))
+      .subscribe(posts => {
+        this.loadedPosts = posts
+        this.isFetching = false
+      })
   }
 }
