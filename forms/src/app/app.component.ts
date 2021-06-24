182a33611005eb2371df9d024e0e1d2274d305ad
Jon Thornton <jon.thornton07@gmail.com>
2021-06-24 18:17:35 -0400

    Section 15: End

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -9,11 +9,12 @@ import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
 export class AppComponent {
   genders = ['male', 'female']
   signupForm: FormGroup
+  forbiddenUsernames = ['jon', 'test']
 
   constructor() {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
         'email': new FormControl(null, [Validators.required, Validators.email]),
       }),
       'gender': new FormControl('male'),
@@ -33,5 +34,12 @@ export class AppComponent {
   onAddHobby() {
     const control = new FormControl(null, Validators.required);
     (<FormArray>this.signupForm.get('hobbies')).push(control)
+  }
+
+  forbiddenNames(control: FormControl): {[s: string]: boolean} | null  {
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return { 'nameIsForbidden': true }
+    }
+    return null
   }
 }
