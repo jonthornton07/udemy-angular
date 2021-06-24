0064ec7a3d595a84b2c0efa1d98a3cbaf5571ce9
jonthornton07 <jon.thornton07@gmail.com>
2021-06-24 09:15:37 -0400

    Adding in some forms

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -12,13 +12,40 @@ export class AppComponent {
   defaultQuestion = 'teacher'
   answer = ''
   genders = ['male', 'female']
+  user = {
+    username: '',
+    email: '',
+    question: '',
+    answer: '',
+    gender: ''
+  }
+  submitted = false
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // })
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    })
   }
 
   onSubmit() {
-    console.log(this.signupForm)
+    this.submitted = true
+    this.user.username = this.signupForm.value.userData.username
+    this.user.email = this.signupForm.value.userData.email
+    this.user.question = this.signupForm.value.sercret
+    this.user.answer = this.signupForm.value.questionAnswer
+    this.user.username = this.signupForm.value.gender
   }
 
   // onSubmit(f: NgForm) {
